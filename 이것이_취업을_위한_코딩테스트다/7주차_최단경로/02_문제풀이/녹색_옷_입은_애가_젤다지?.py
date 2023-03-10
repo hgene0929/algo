d82da32b8c5e59dc22e5c22013d90f56d063ac1e
hgene0929 <hgene0929@gmail.com>
2023-03-10 14:12:54 +0900

    ✨ [이것이 취업을 위한 코딩 테스트다:7주차 최단 경로] 녹색 옷 입은 애가 젤다지?

---
+++ 이것이_취업을_위한_코딩테스트다/7주차_최단경로/02_문제풀이/녹색_옷_입은_애가_젤다지?.py
@@ -0,0 +1,34 @@
+import sys, heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+#풀이
+moves = [(-1,0),(1,0),(0,-1),(0,1)]
+
+def get_shortest_distance(graph, distance, n):
+    q = []
+    heapq.heappush(q, (graph[0][0],0,0))
+    distance[0][0] = graph[0][0]
+    while q:
+        dist,x,y = heapq.heappop(q)
+        if distance[x][y] < dist:
+            continue
+        for move in moves:
+            if 0 <= x+move[0] < n and 0 <= y+move[1] < n:
+                nx,ny = x+move[0],y+move[1]
+                cost = dist + graph[nx][ny]
+                if cost < distance[nx][ny]:
+                    distance[nx][ny] = cost
+                    heapq.heappush(q, (cost,nx,ny))
+    return distance[n-1][n-1]
+
+#테스트케이스
+idx = 1
+n = int(input())
+while n != 0:
+    graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
+    distance = [[INF]*n for _ in range(n)]
+    print(f'Problem {idx}: {get_shortest_distance(graph,distance,n)}')
+    idx += 1
+    n = int(input())
