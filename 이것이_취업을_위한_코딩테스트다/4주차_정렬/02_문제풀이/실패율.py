086b8919eb357c6aaa4981503eb0218d854e5a10
hgene0929@gmail.com <hgene0929@gmail.com>
2023-04-14 16:10:20 +0900

    ✨ 이것이 취업을 위한 코딩 테스트다:4주차 정렬] 실패율

--- 이것이_취업을_위한_코딩테스트다/4주차_정렬/02_문제풀이/실패율.py
+++ 이것이_취업을_위한_코딩테스트다/4주차_정렬/02_문제풀이/실패율.py
@@ -3,19 +3,18 @@ import heapq
 def solution(N, stages):
     stages.sort()
     
-    remains = [0] * (N+2)
+    remains = [0] * (N+2) #현재 단계에 남아있는 사람 수
     for stage in stages:
         remains[stage] += 1
-    remains = remains[1:-1]
     
     q = []
-    cleared = len(stages)
-    for idx,remain in enumerate(remains):
-        if cleared == 0:
+    cleared = len(stages) #현재 단계에 도달한 사람 수
+    for idx,remain in enumerate(remains[1:-1]):
+        if cleared == 0: #스테이지에 도달한 사람이 한명도 없다면 -> 실패율 == 0
             heapq.heappush(q, (0, idx+1))
         else:
-            heapq.heappush(q, (-(remain/cleared),idx+1))
-        cleared -= remain
+            heapq.heappush(q, (-(remain/cleared),idx+1)) #실패율 = 현재 단계에 남아있는 사람 수 / 현재 단계에 도달한 사람 수
+        cleared -= remain #현재 단계에 남아있는 사람은 다음 단게에 도달하지 X
     
     results = []
     while q:
